71378b3219ace444329d6c28d2505aff357cdeff
soyoung125 <sypark041754@gmail.com>
2022-04-02 23:38:20 +0900

    Merge branch 'main' of https://github.com/gabrielyoon7/GIDAL

--- app/src/views/diary/DiaryListView.js
+++ app/src/views/diary/DiaryListView.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-const DiaryListView = () =>{
+const DiaryListView = (props) =>{
+    const items = [{
+        '2022-04-22': { name: 'item 1 - any js object' },
+        '2022-04-23': { name: 'item 2 - any js object', height: 80 },
+        '2022-04-24': {},
+        '2022-04-25': { name: 'item 3 - any js object' }
+    }]
+    const [data, setData] = useState('');
+
+    useEffect(() => {
+        items.filter(function (obj) {
+            console.log(obj[props.selectedDate].name);
+            setData(obj[props.selectedDate].name);
+        });
+    })
+
     return (
         <View style={styles.container}>
-            <Text>다이어리 리스트 뷰</Text>
+            <Text>{data}</Text>
         </View>
     )
 }

bf5a93b0d4bf17d2dfac834c887e40b58bb18e52
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-04-03 03:16:41 +0900

    .

--- app/App.js
+++ app/App.js
@@ -1,30 +1,29 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MainRoute from './src/routes/main/MainRoute';
-// import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
-import { SafeAreaView} from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
+// import { SafeAreaView} from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider } from "native-base";
 import 'react-native-gesture-handler';
 
 
 export default function App() {
   return (
-    // <SafeAreaView style={styles.AndroidSafeArea}>
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <MainRoute/>
-        {/* <StatusBar style="auto" /> */}
-      </NavigationContainer>
-    </NativeBaseProvider>
-    // </SafeAreaView>
+    <SafeAreaView style={styles.AndroidSafeArea}>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <MainRoute/>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </SafeAreaView>
   );
 }
 
-// const styles = StyleSheet.create({
-//   AndroidSafeArea: {
-//     flex: 1,
-//     backgroundColor: "white",
-//     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
-//   },
-// });
+const styles = StyleSheet.create({
+  AndroidSafeArea: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+});

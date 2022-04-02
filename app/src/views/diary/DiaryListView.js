0f61a0c691b3d7730769c95d0f64de59b3d791e8
soyoung125 <sypark041754@gmail.com>
2022-04-03 00:44:25 +0900

    다이어리 리스트 추가(initialIndex가 적용안되는 문제 있음)

--- app/src/views/diary/DiaryListView.js
+++ app/src/views/diary/DiaryListView.js
@@ -1,25 +1,59 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, View, StatusBar, StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+const Item = ({ item, onPress, backgroundColor, textColor }) => (
+    <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+        <Text>{item.date}</Text>
+        <Text style={[styles.title, textColor]}>{item.title}</Text>
+    </TouchableOpacity>
+);
+  
 
 const DiaryListView = (props) =>{
-    const items = [{
-        '2022-04-22': { name: 'item 1 - any js object' },
-        '2022-04-23': { name: 'item 2 - any js object', height: 80 },
-        '2022-04-24': {},
-        '2022-04-25': { name: 'item 3 - any js object' }
-    }]
-    const [data, setData] = useState('');
+    const items = [
+        { id:"1",  title: 'item 1 - any js object', date: '2022-04-22' },
+        { id:"2",  title: 'item 2 - any js object', date: '2022-04-23' },
+        { id:"3",  title: 'no name', date: '2022-04-23' },
+        { id:"4",  title: 'item 3 - any js object', date: '2022-04-24' },
+        { id:"5",  title: 'item 1 - any js object', date: '2022-04-25' },
+        { id:"6",  title: 'item 2 - any js object', date: '2022-04-26' },
+        { id:"7",  title: 'no name', date: '2022-04-26' },
+        { id:"8",  title: 'item 3 - any js object', date: '2022-04-27' }
+    ]
+    const [data, setData] = useState(0);
+    const [selectedId, setSelectedId] = useState(null);
 
     useEffect(() => {
-        items.filter(function (obj) {
-            console.log(obj[props.selectedDate].name);
-            setData(obj[props.selectedDate].name);
-        });
+        const index = items.findIndex((item, idx) => {
+            return item.date === props.selectedDate
+        })
+        setData(index);
     })
+
+    const renderItem = ({ item }) => {
+        const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+        const color = item.id === selectedId ? 'white' : 'black';
+
+        return (
+            <Item
+                item={item}
+                onPress={() => setSelectedId(item.id)}
+                // 해당 일기로 넘어가기 구현
+                backgroundColor={{ backgroundColor }}
+                textColor={{ color }}
+            />
+        );
+    };
 
     return (
         <View style={styles.container}>
-            <Text>{data}</Text>
+            <FlatList
+                data={items}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id}
+                extraData={selectedId}
+                initialScrollIndex ={data}
+            />
         </View>
     )
 }
@@ -27,10 +61,18 @@ export default DiaryListView;
 
 const styles = StyleSheet.create({
     container: {
-      flex: 0.5,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 0.5,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-  });
+    item: {
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
+});
   

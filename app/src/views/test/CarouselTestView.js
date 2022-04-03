f2d5b1fea5dcc3d5176f2758a05aeac79438b3f2
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-04-04 03:03:45 +0900

    태그 질문 타입 추가

--- app/src/views/test/CarouselTestView.js
+++ app/src/views/test/CarouselTestView.js
@@ -4,135 +4,162 @@ import { Button, View, Text, SafeAreaView, ScrollView, StyleSheet, Pressable, Al
 import { Center } from 'native-base';
 
 const exampleItems = [
-    {
-      question: '어떤 종류의 일기인가요?',
-      tags: [{
-        id: 1,
-        name: 'a'
-       }, {
-        id: 2,
-        name: 'b'
-        }]
-      },
-    {
-      question: '오늘 하루 기분이 어땠나요?',
-      tags: [{
-        id: 3,
-        name: 'c'
-       }, {
-        id: 4,
-        name: 'd'
-        }]
-      },
-    {
-      question: '오늘 날씨는 어땠나요?',
-      tags: [{
-        id: 5,
-        name: 'e'
-       }, {
-        id: 6,
-        name: 'f'
-        }]
-      },
-    {
-      question: '일기를 작성하는 위치가 어디인가요?',
-      tags: [{
-        id: 7,
-        name: 'g'
-       }, {
-        id: 8,
-        name: 'h'
-        }]
-      },
-    {
-      question: '오늘의 수면시간',
-      tags: [{
-        id: 9,
-        name: 'i'
-       }, {
-        id: 10,
-        name: 'j'
-        }]
-      },
-  ];
-  
-  const CustomCarousel = () => {
-    const [activeIndex, setActiveIndex] = useState(0);
-    const [carouselItems, setCarouselItems] = useState(exampleItems);
-    const ref = useRef(null);
+  {
+    type : 'button',
+    question: '버튼형 질문',
+    tags: [{
+      id: 1,
+      name: 'a'
+    }, {
+      id: 2,
+      name: 'b'
+    }, {
+      id: 3,
+      name: 'b'
+    }]
+  },
+  {
+    type : 'search',
+    question: '검색형 질문',
+    tags: [{
+      id: 1,
+      name: 'a'
+    }, {
+      id: 2,
+      name: 'b'
+    }, {
+      id: 3,
+      name: 'b'
+    }]
+  },
+  {
+    type : 'time',
+    question: '시간형 질문',
+    tags: [{
+      id: 1,
+      name: 'a'
+    }, {
+      id: 2,
+      name: 'b'
+    }, {
+      id: 3,
+      name: 'b'
+    }]
+  },
+  {
+    type : 'slider1',
+    question: '수치형 질문1',
+    tags: [{
+      id: 1,
+      name: 'a'
+    }, {
+      id: 2,
+      name: 'b'
+    }, {
+      id: 3,
+      name: 'b'
+    }]
+  },
+  {
+    type : 'slider2',
+    question: '수치형 질문2',
+    tags: [{
+      id: 1,
+      name: 'a'
+    }, {
+      id: 2,
+      name: 'b'
+    }, {
+      id: 3,
+      name: 'b'
+    }]
+  },
+];
 
-    const onPressFunction = () => {
-      Alert.alert('press!')
-    }
-  
-    const renderItem = useCallback(({ item, index }) => (
-      <View style={{ backgroundColor: '#dcdde1', marginTop: 20, borderRadius: 10}}>
+const ButtonView = (tag) =>{
 
-        <Text style={{ fontSize: 20, fontWeight: 'bold', margin: 10 }}>{item.question}</Text>
-        <View style={{ alignItems: 'center', justifyContent: 'center'}}>
-          <View style={styles.lineView}/>
-        </View>
-        <View style={{ justifyContent: 'center', flexDirection : "row" }} >
+}
+
+const CustomCarousel = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [carouselItems, setCarouselItems] = useState(exampleItems);
+  const ref = useRef(null);
+
+  const onPressFunction = (tag) => {
+    Alert.alert(tag+' pressed!')
+  }
+
+  const renderItem = useCallback(({ item, index }) => (
+    <View style={{ backgroundColor: '#dcdde1', marginTop: 20, borderRadius: 10 }}>
+
+      <Text style={{ fontSize: 20, fontWeight: 'bold', margin: 10 }}>{item.question}</Text>
+      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <View style={styles.lineView} />
+      </View>
+      <View style={{ justifyContent: 'center', flexDirection: "row" }} >
         {item.tags.map((tag) => (
           <View key={tag.id} style={styles.btnView}>
-            <Pressable style={styles.button} onPress={onPressFunction}>
-                <Text>{tag.name}</Text>
+            <Pressable style={styles.button} onPress={ () => onPressFunction(tag.name) }>
+              <Text>{tag.name}</Text>
             </Pressable>
-            </View>
+          </View>
         ))}
-        </View>
-        
       </View>
-    ), []);
-  
-    return (
-        <View>
-          <Carousel
-            layout="default"
-            ref={ref}
-            data={carouselItems}
-            sliderWidth={350}
-            itemWidth={350}
-            renderItem={renderItem}
-            onSnapToItem={(index) => setActiveIndex(index)}
-          />
-        </View>
-    );
-  };
-  
-  const styles = StyleSheet.create({
-    button: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 8,
-      borderRadius: 100,
-      backgroundColor: '#dcdde1',
-      width: 80
-    },
-    btnView: {
-      borderWidth: StyleSheet.hairlineWidth,
-      borderColor: 'black',
-      borderRadius: 100,
-      borderWidth: 2,
-      margin: 5,
-      marginTop: 15
-    },
-    lineView: {
-      borderBottomColor: 'black',
-      borderBottomWidth: 3,
-      width: '90%',
-    }
-  });
-  
+
+    </View>
+  ), []);
+
+  return (
+    <View>
+      <Carousel
+        layout="default"
+        ref={ref}
+        data={carouselItems}
+        sliderWidth={350}
+        itemWidth={350}
+        renderItem={renderItem}
+        onSnapToItem={(index) => setActiveIndex(index)}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 8,
+    borderRadius: 100,
+    backgroundColor: '#dcdde1',
+    width: 80
+  },
+  btnView: {
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: 'black',
+    borderRadius: 100,
+    borderWidth: 2,
+    margin: 5,
+    marginTop: 15
+  },
+  lineView: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 3,
+    width: '90%',
+  }
+});
+
 
 
 const CarouselTestView = () => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <CustomCarousel />
-        </View>
+  return (
+    <View style={{ flex: 1 }}>
+      <ScrollView><></></ScrollView>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <CustomCarousel />
+      </View>
+    </View>
 
-    )
+  )
 }
 
 export default CarouselTestView;

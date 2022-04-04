776aebeb02cac35c5ded5bf5d961d240aae605b2
김가영 <83538598+gykim0923@users.noreply.github.com>
2022-04-05 00:56:16 +0900

    일기쓰기 UI구현중

--- app/src/views/diary/write/DiaryWriteView.js
+++ app/src/views/diary/write/DiaryWriteView.js
@@ -1,11 +1,45 @@
 import * as React from 'react';
-import { FlatList, View, StatusBar, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { FlatList, View, StatusBar, StyleSheet, Text, TouchableOpacity, TextInput } from 'react-native';
+import CalendarView from '../../../../src/views/diary/list/CalendarView';
+import  { useState, useEffect, Component } from 'react'; 
+import { Box, Input, Button, Modal, Center, NativeBaseProvider } from "native-base"
+const InputTitle = () => {
+    return <Box alignItems="center">
+        <Input mx="3" placeholder="title" w="75%" maxWidth="300px" />
+      </Box>;
+  };
 
-
+  const WriteDiaryButton = () => {
+    return <Box alignItems="center">
+    <Button onPress={() => console.log("hello world")}>작성하기</Button>
+  </Box>;
+  };
 
 const DiaryWriteView = (props) => {
+   
+      const [Date,setDate] = useState();
+      const [Title,setTitle] = useState();
+      
     return (
-        <Text>write</Text>
+       
+            <>
+            {/* <TextInput
+                placeholder="제목을 입력하세요"
+            
+                onChangeTitle ={(Title)=>this.setTitle(Title: Title)}
+                    }}
+                /> */}
+            <Text>2022.04.05</Text>
+            <Text>Title</Text>
+            <InputTitle />
+            <WriteDiaryButton/>
+            </>
+           
+
+
+        
+        
+
     )
 }
 

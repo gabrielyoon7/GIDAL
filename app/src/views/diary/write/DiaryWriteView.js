3b35e878a47f33de57997155dfd908e4ec06c01d
김가영 <83538598+gykim0923@users.noreply.github.com>
2022-04-05 19:32:37 +0900

    DiaryWriteView날짜 모달 수정UI 구현

--- app/src/views/diary/write/DiaryWriteView.js
+++ app/src/views/diary/write/DiaryWriteView.js
@@ -3,35 +3,56 @@ import { FlatList, View, StatusBar, StyleSheet, Text, TouchableOpacity, TextInpu
 import CalendarView from '../../../../src/views/diary/list/CalendarView';
 import  { useState, useEffect, Component } from 'react'; 
 import { Box, Input, Button, Modal, Center, NativeBaseProvider } from "native-base"
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+
 const InputTitle = (props) => {
     return <Box alignItems="center">
         <Input mx="3" placeholder="title" w="75%" maxWidth="300px" onChangeText={(title)=>{props.setTitle(title);}} />
       </Box>;
   };
 
+  
 
+ 
 const DiaryWriteView = (props) => {
    
       const [Date,setDate] = useState('');
       const [Title,setTitle] = useState('');
+      const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
 
-      const WriteDiaryButton = () => {
+      const showDatePicker = () => {
+        setDatePickerVisibility(true);
+    };
+  
+    const hideDatePicker = () => {
+        setDatePickerVisibility(false);
+    };
+  
+    const handleConfirm = (date) => {
+        console.warn("A date has been picked: ", date);
+        hideDatePicker();
+    };
+  
+  
+    const WriteDiaryButton = () => {
         return <Box alignItems="center">
         <Button onPress={() => console.log(Title)}>작성하기</Button>
       </Box>;
       };
     return (
-       
+
             <>
-            {/* <TextInput
-                placeholder="제목을 입력하세요"
-            
-                onChangeTitle ={(Title)=>this.setTitle(Title)}
-                    
-                /> */}
-            <Text>2022.04.05</Text>
+       
+            <Text  title="Show Date Picker" ShonPress={showDatePicker} >날짜 어떻게 오는지 모르겠음</Text>
             <Text>Title</Text>
             <InputTitle  setTitle={setTitle} Title={Title} />
+           
+            <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                onConfirm={handleConfirm}
+                onCancel={hideDatePicker}
+            />
             <WriteDiaryButton />
             </>
            

6598f4116bd4cf97af88827cc8488ab7a403795c
soyoung125 <sypark041754@gmail.com>
2022-04-05 20:36:35 +0900

    다이어리 리스트뷰 선택한 날짜에 맞게 스크롤링 구현

--- app/src/views/diary/list/DiaryListView.js
+++ app/src/views/diary/list/DiaryListView.js
@@ -7,10 +7,11 @@ import { AntDesign } from "@expo/vector-icons";
 
 const DiaryListView = (props) => {
     const [date, setSelectedDate] = React.useState(props.selectedDate);    
+    
     return (
         <>
-            <CalendarView selectedDate={date } setSelectedDate={setSelectedDate} />
-            <DiaryList selectedDate={props.selectedDate} navigation={props.navigation} />
+            <CalendarView selectedDate={date} setSelectedDate={setSelectedDate} />
+            <DiaryList selectedDate={date} navigation={props.navigation} />
             <Fab renderInPortal={false} shadow={2} size="md" 
                 icon={<Icon color="white" as={AntDesign} name="plus" size="md" />} 
                 onPress={() => props.navigation.navigate('DiaryWrite', {selectedDate: date})}

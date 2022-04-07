9d29b338b7c410a7b7b6f10ad5b33bae8fd7c9c8
soyoung125 <sypark041754@gmail.com>
2022-04-08 00:30:09 +0900

    다이어리 읽기 페이지 ui작업

--- app/src/views/diary/read/DiaryReadView.js
+++ app/src/views/diary/read/DiaryReadView.js
@@ -1,12 +1,60 @@
+import { Box, Input, Text, TextArea, Stack, Button } from 'native-base';
 import * as React from 'react';
-import { FlatList, View, StatusBar, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 
 
 const DiaryReadView = (props) => {
     return (
-        <Text>read</Text>
+        <>
+            <Text style={styles.dateText} >Date</Text>
+            <Text style={styles.textStyle} >Title</Text>
+            <Box alignItems="center" w="100%">
+                <Input mx="3" variant="underlined" value="title" w="75%" isDisabled  />
+            </Box>
+            <Text style={styles.textStyle} >Content</Text>
+            <Box alignItems="center" w="100%" >
+                <TextArea h="45%" value="content" w="75%" isDisabled  />
+            </Box>
+
+            <Stack mb="2.5" mt="1.5" direction={{
+                base: "column",
+                md: "row"
+            }} space={2} mx={{
+                base: "auto",
+                md: "0"
+            }}>
+                <Button size="md" variant="outline">
+                    수정
+                </Button>
+                <Button size="md" variant="outline" colorScheme="secondary">
+                    삭제
+                </Button>
+            </Stack>
+        </>
     )
 }
+
+const styles = StyleSheet.create({
+    dateText: {
+  
+      fontWeight: "bold",
+      textAlign: "center",
+      fontSize: 30,
+    },
+    textStyle: {
+  
+      fontWeight: "bold",
+      textAlign: "left",
+      fontSize: 17,
+      marginLeft: 50,
+      margin: 10,
+  
+    },
+    buttonContainer: {
+      // margin: 20,
+  
+    },
+  })
 
 export default DiaryReadView;

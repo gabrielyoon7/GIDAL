7a024d2936bab910c88529e90906c3057c010c52
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-04-08 14:52:21 +0900

    SearchBar 재활성화

--- app/src/views/sns/feed/FeedSearchView.js
+++ app/src/views/sns/feed/FeedSearchView.js
@@ -3,42 +3,17 @@ import { config } from '../../../../config'
 import SearchBar from "react-native-dynamic-search-bar";
 
 
-const handleOnChangeText = (text) => {
-    // ? Visible the spinner
-    setState({
-      searchText: text,
-      spinnerVisibility: true,
-    });
-
-    // ? After you've done to implement your use-case
-    // ? Do not forget to set false to spinner's visibility
-    setState({
-      spinnerVisibility: false,
-    });
-  };
-
-
 const FeedSearchView = () => {
 
-    const { spinnerVisibility } = this.state;
-    return (
-        <View>
-          <SearchBar
-            height={50}
-            fontSize={24}
-            fontColor="#fdfdfd"
-            iconColor="#fdfdfd"
-            shadowColor="#282828"
-            cancelIconColor="#fdfdfd"
-            backgroundColor="#ba312f"
-            spinnerVisibility={spinnerVisibility}
-            placeholder="Search any cosmetics ..."
-            fontFamily="BurbankBigCondensed-Black"
-            shadowStyle={styles.searchBarShadowStyle}
-            onChangeText={handleOnChangeText}
-          />
-        </View>
-      );
+  return (
+    <View>
+      <SearchBar
+        placeholder="Search here"
+        onPress={() => alert("onPress")}
+        onChangeText={(text) => console.log(text)}
+      />
+    </View>
+  );
 }
 
 export default FeedSearchView;

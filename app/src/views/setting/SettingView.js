043d36477db87e1284cbc869ac1c58f903738bcf
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-04-17 15:11:59 +0900

    설정 페이지 디자인 개선 및 링크 기능 추가

--- app/src/views/setting/SettingView.js
+++ app/src/views/setting/SettingView.js
@@ -1,19 +1,7 @@
-import { Button } from 'native-base';
+// import {  } from 'native-base';
+import { Center, Container, Heading, Link } from 'native-base';
 import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        //    paddingTop: 22
-    },
-    item: {
-        padding: 10,
-        fontSize: 18,
-        height: 44,
-    },
-});
+import { Button, FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 const menu = [
     { key: 0, menu: '테스트 페이지', nav: 'Test' },
@@ -22,9 +10,39 @@ const menu = [
     { key: 3, menu: '앱 소개', nav: 'Test' },
 ]
 
+const SettingHeader = () => {
+    return (
+        <Center flex={1}>
+            <Container>
+                <Heading>
+                    경기대학교
+                </Heading>
+                <Heading>
+                    2022 컴퓨터공학심화캡스톤
+                </Heading>
+                <Heading color="emerald.400">
+                    기록의 달인
+                </Heading>
+                <Heading pt={4} fontWeight="500" size="sm">
+                    통계와 SNS를 기반으로 한 일기작성 SNS
+                </Heading>
+                <Link href="https://github.com/gabrielyoon7/GIDAL" isExternal _text={{
+                    color: "blue.400"
+                }} mt={-0.5} _web={{
+                    mb: -2
+                }}>
+                    Github README
+                </Link>
+            </Container>
+
+        </Center>
+    );
+} // Example template which wraps component with NativeBaseProvider
+
 const SettingView = (props) => {
     return (
         <>
+            <SettingHeader />
             <View style={styles.container}>
                 <FlatList
                     data={menu}
@@ -40,13 +58,26 @@ const SettingView = (props) => {
                         )}
                 />
             </View>
-            <Button
+            {/* <Button
                 title="테스트 메뉴로 이동하기"
                 onPress={() => props.navigation.navigate('Test')}
-            />
-
+            /> */}
         </>
     );
 }
 
 export default SettingView;
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 30,
+        //    paddingTop: 22
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+});

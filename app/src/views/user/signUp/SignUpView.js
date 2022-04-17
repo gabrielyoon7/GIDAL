d22875e74ab4e65457a2cd9f0e395430e8d4e88e
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-04-17 14:43:55 +0900

    회원가입 시 스크롤이 안되는 문제 해결

--- app/src/views/user/signUp/SignUpView.js
+++ app/src/views/user/signUp/SignUpView.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Box, Heading, VStack, FormControl, Input, Button, Center, NativeBaseProvider } from "native-base";
+import { Box, Heading, VStack, FormControl, Input, Button, Center, NativeBaseProvider, ScrollView } from "native-base";
 import { useState } from "react";
 import { config } from '../../../../config'
 
@@ -13,37 +13,37 @@ const SignUpView = (props) => {
     const [residence, setResidence] = useState('');
 
     const signUp = () => {
-        if(!id){
+        if (!id) {
             alert('아이디를 입력해주세요');
             return;
         }
-        if(!password){
+        if (!password) {
             alert('비밀번호를 입력해주세요');
             return;
         }
-        if(!checkPassword){
+        if (!checkPassword) {
             alert('비밀번호 확인을 입력해주세요');
             return;
         }
-        if(password !== checkPassword){
+        if (password !== checkPassword) {
             alert('비밀번호가 일치하지 않습니다.');
             console.log(password);
             console.log(checkPassword);
             return;
         }
-        if(!name){
+        if (!name) {
             alert('이름을 입력해주세요');
             return;
         }
-        if(!birthDay){
+        if (!birthDay) {
             alert('생년월일을 입력해주세요');
             return;
         }
-        if(!gender){
+        if (!gender) {
             alert('성별을 입력해주세요');
             return;
         }
-        if(!residence){
+        if (!residence) {
             alert('거주지를 입력해주세요');
             return;
         }
@@ -57,7 +57,7 @@ const SignUpView = (props) => {
                 location: residence
             }
         }).then((response) => {
-            if (response.data.status === 'success'){
+            if (response.data.status === 'success') {
                 props.navigation.pop()
             } else {
                 alert('이미 존재하는 아이디입니다.');
@@ -68,53 +68,55 @@ const SignUpView = (props) => {
     }
 
     return (
-        <Center w="100%" h="100%">
-            <Box safeArea p="2" w="90%" maxW="290" py="8">
-                <Heading size="lg" color="coolGray.800" _dark={{
-                    color: "warmGray.50"
-                }} fontWeight="semibold">
-                    Welcome
-                </Heading>
-                <Heading mt="1" color="coolGray.600" _dark={{
-                    color: "warmGray.200"
-                }} fontWeight="medium" size="xs">
-                    Sign up to continue!
-                </Heading>
-                <VStack space={3} mt="5">
-                    <FormControl>
-                        <FormControl.Label>아이디</FormControl.Label>
-                        <Input onChangeText={(value) => setUserId(value)} />
-                    </FormControl>
-                    <FormControl>
-                        <FormControl.Label>비밀번호</FormControl.Label>
-                        <Input type="password" onChangeText={(value) => setPassword(value)} />
-                    </FormControl>
-                    <FormControl>
-                        <FormControl.Label>비밀번호 확인</FormControl.Label>
-                        <Input type="password" onChangeText={(value) => setCheckPassword(value)} />
-                    </FormControl>
-                    <FormControl>
-                        <FormControl.Label>이름</FormControl.Label>
-                        <Input onChangeText={(value) => setName(value)} />
-                    </FormControl>
-                    <FormControl>
-                        <FormControl.Label>생년월일(추후 모달 구현 예정)</FormControl.Label>
-                        <Input onChangeText={(value) => setBirthDay(value)} />
-                    </FormControl>
-                    <FormControl>
-                        <FormControl.Label>성별(추후 라디오 버튼 추가할 예정)</FormControl.Label>
-                        <Input onChangeText={(value) => setGender(value)} />
-                    </FormControl>
-                    <FormControl>
-                        <FormControl.Label>거주지</FormControl.Label>
-                        <Input onChangeText={(value) => setResidence(value)} />
-                    </FormControl>
-                    <Button mt="2" colorScheme="indigo" onPress={() => signUp()} >
-                        Sign up
-                    </Button>
-                </VStack>
-            </Box>
-        </Center>
+        <ScrollView>
+            <Center w="100%" h="100%">
+                <Box safeArea p="2" w="90%" maxW="290" py="8">
+                    <Heading size="lg" color="coolGray.800" _dark={{
+                        color: "warmGray.50"
+                    }} fontWeight="semibold">
+                        Welcome
+                    </Heading>
+                    <Heading mt="1" color="coolGray.600" _dark={{
+                        color: "warmGray.200"
+                    }} fontWeight="medium" size="xs">
+                        Sign up to continue!
+                    </Heading>
+                    <VStack space={3} mt="5">
+                        <FormControl>
+                            <FormControl.Label>아이디</FormControl.Label>
+                            <Input onChangeText={(value) => setUserId(value)} />
+                        </FormControl>
+                        <FormControl>
+                            <FormControl.Label>비밀번호</FormControl.Label>
+                            <Input type="password" onChangeText={(value) => setPassword(value)} />
+                        </FormControl>
+                        <FormControl>
+                            <FormControl.Label>비밀번호 확인</FormControl.Label>
+                            <Input type="password" onChangeText={(value) => setCheckPassword(value)} />
+                        </FormControl>
+                        <FormControl>
+                            <FormControl.Label>이름</FormControl.Label>
+                            <Input onChangeText={(value) => setName(value)} />
+                        </FormControl>
+                        <FormControl>
+                            <FormControl.Label>생년월일(추후 모달 구현 예정)</FormControl.Label>
+                            <Input onChangeText={(value) => setBirthDay(value)} />
+                        </FormControl>
+                        <FormControl>
+                            <FormControl.Label>성별(추후 라디오 버튼 추가할 예정)</FormControl.Label>
+                            <Input onChangeText={(value) => setGender(value)} />
+                        </FormControl>
+                        <FormControl>
+                            <FormControl.Label>거주지</FormControl.Label>
+                            <Input onChangeText={(value) => setResidence(value)} />
+                        </FormControl>
+                        <Button mt="2" colorScheme="indigo" onPress={() => signUp()} >
+                            Sign up
+                        </Button>
+                    </VStack>
+                </Box>
+            </Center>
+        </ScrollView>
     );
 }
 

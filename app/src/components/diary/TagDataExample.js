9bd592d17150658e5cbabd1e6bcad314e6be884b
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-04-19 23:49:03 +0900

    tag 셀렉터 수정을 위한 사전 작업 진행중

---
+++ app/src/components/diary/TagDataExample.js
@@ -0,0 +1,76 @@
+export const TagDataExample = [
+    {
+        type: 'button',
+        question: '어떤 종류의 일기인가요?',
+        tags: [
+            {
+                id: 1,
+                name: 'a'
+            }, 
+            {
+                id: 2,
+                name: 'b'
+            }, 
+            {
+                id: 3,
+                name: 'b'
+            }
+        ]
+    },
+    {
+        type: 'search',
+        question: '검색형 질문',
+        tags: [{
+            id: 1,
+            name: 'a'
+        }, {
+            id: 2,
+            name: 'b'
+        }, {
+            id: 3,
+            name: 'b'
+        }]
+    },
+    {
+        type: 'time',
+        question: '시간형 질문',
+        tags: [{
+            id: 1,
+            name: 'a'
+        }, {
+            id: 2,
+            name: 'b'
+        }, {
+            id: 3,
+            name: 'b'
+        }]
+    },
+    {
+        type: 'slider1',
+        question: '수치형 질문1',
+        tags: [{
+            id: 1,
+            name: 'a'
+        }, {
+            id: 2,
+            name: 'b'
+        }, {
+            id: 3,
+            name: 'b'
+        }]
+    },
+    {
+        type: 'slider2',
+        question: '수치형 질문2',
+        tags: [{
+            id: 1,
+            name: 'a'
+        }, {
+            id: 2,
+            name: 'b'
+        }, {
+            id: 3,
+            name: 'b'
+        }]
+    },
+];
--- app/src/components/diary/TagSelector.js
+++ app/src/components/diary/TagSelector.js
@@ -2,80 +2,10 @@ import { View } from "native-base";
 import { useCallback, useRef, useState } from "react";
 import Carousel from "react-native-snap-carousel";
 import TagCard from "../tag/TagCard";
+import { TagDataExample } from "./TagDataExample";
 
 const TagSelector = (props) =>{
-    const exampleItems = [
-      {
-        type : 'button',
-        question: '버튼형 질문',
-        tags: [{
-          id: 1,
-          name: 'a'
-        }, {
-          id: 2,
-          name: 'b'
-        }, {
-          id: 3,
-          name: 'b'
-        }]
-      },
-      {
-        type : 'search',
-        question: '검색형 질문',
-        tags: [{
-          id: 1,
-          name: 'a'
-        }, {
-          id: 2,
-          name: 'b'
-        }, {
-          id: 3,
-          name: 'b'
-        }]
-      },
-      {
-        type : 'time',
-        question: '시간형 질문',
-        tags: [{
-          id: 1,
-          name: 'a'
-        }, {
-          id: 2,
-          name: 'b'
-        }, {
-          id: 3,
-          name: 'b'
-        }]
-      },
-      {
-        type : 'slider1',
-        question: '수치형 질문1',
-        tags: [{
-          id: 1,
-          name: 'a'
-        }, {
-          id: 2,
-          name: 'b'
-        }, {
-          id: 3,
-          name: 'b'
-        }]
-      },
-      {
-        type : 'slider2',
-        question: '수치형 질문2',
-        tags: [{
-          id: 1,
-          name: 'a'
-        }, {
-          id: 2,
-          name: 'b'
-        }, {
-          id: 3,
-          name: 'b'
-        }]
-      },
-    ];
+    const exampleItems = TagDataExample;
     
     const [activeIndex, setActiveIndex] = useState(0);
     const [carouselItems, setCarouselItems] = useState(exampleItems);

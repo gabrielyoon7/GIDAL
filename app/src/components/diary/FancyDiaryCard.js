e8bbf480a025b9d5d4fd804148b0974159116dad
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-04-28 02:24:56 +0900

    카드 터치감 전면 개선

--- app/src/components/diary/FancyDiaryCard.js
+++ app/src/components/diary/FancyDiaryCard.js
@@ -1,5 +1,5 @@
 import { StyleSheet, TouchableOpacity, } from 'react-native';
-import { Badge, Box, HStack, Spacer, Text, } from 'native-base';
+import { Badge, Box, Flex, HStack, Pressable, Spacer, Text, } from 'native-base';
 import { Ionicons } from "@expo/vector-icons";
 
 const FancyDiaryCard = ({ item: diary, onPress, backgroundColor, textColor }) => {
@@ -8,43 +8,98 @@ const FancyDiaryCard = ({ item: diary, onPress, backgroundColor, textColor }) =>
     content = content.replace(/<br\/>/ig, "\n");
     content = content.replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, "");
     content = content.replace(/(<([^>]+)>)/gi, "");
-    content = content.replace(/&nbsp;/gi,"");
+    content = content.replace(/&nbsp;/gi, "");
     // 정규식을 이용한 HTML 태그 제거 끝
     return (
-        <TouchableOpacity onPress={onPress} style={[styles.diary, backgroundColor]}>
-            <Box maxW="96" borderWidth="1" borderColor="coolGray.300" shadow="3" bg="coolGray.100" p="5" rounded="8">
-                <HStack alignItems="center">
-                    <Badge colorScheme="green" _text={{
-                        color: "white"
-                    }} variant="solid" rounded="4">
-                        {diary.disclosure}
-                    </Badge>
-                    <Spacer />
-                    <Text fontSize={10} color="coolGray.800">
-                        {diary.user_id} | {diary.date.substr(0, 10)}
-                    </Text>
-                </HStack>
-                <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
-                    {diary.title}
-                </Text>
-                <Text mt="2" fontSize="sm" color="coolGray.700" numberOfLines={1} ellipsizeMode='tail' width={'90%'}>
-                    {content}
-                </Text>
-                <TouchableOpacity style={styles.heart}>
-                    <Ionicons name="md-heart-outline" size={24} color="grey"/>
-                </TouchableOpacity>
-            </Box>
-            
-        </TouchableOpacity>
-    )
+        <Box alignItems="center" py="1" px="1">
+            <Pressable onPress={onPress}>
+                {({
+                    isHovered,
+                    isFocused,
+                    isPressed
+                }) => {
+                    return (
+                        <Box
+                         maxW="96%"
+                         minW="96%"
+                         borderWidth="1" 
+                         borderColor="coolGray.300" 
+                         shadow="3" 
+                         bg={isPressed ? "coolGray.200" : isHovered ? "coolGray.200" : "coolGray.100"} 
+                         p="5"
+                         rounded="8" 
+                         style={{
+                            transform: [{
+                                scale: isPressed ? 0.96 : 1
+                            }]
+                         }, styles.diary}
+                        >
+                            <HStack alignItems="center">
+                                <Badge colorScheme="green" _text={{
+                                    color: "white"
+                                }} variant="solid" rounded="4">
+                                    {diary.disclosure}
+                                </Badge>
+                                <Spacer />
+                                <Text fontSize={10} color="coolGray.800">
+                                    {diary.user_id} | {diary.date.substr(0, 10)}
+                                </Text>
+                            </HStack>
+                            <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
+                                {diary.title}
+                            </Text>
+                            <Text
+                             mt="2" 
+                             fontSize="sm" 
+                             color="coolGray.700" 
+                             numberOfLines={1} 
+                             ellipsizeMode='tail' 
+                            //  width={'90%'}
+                            >
+                                {content}
+                            </Text>
+                            <TouchableOpacity style={styles.heart}>
+                                <Ionicons name="md-heart-outline" size={24} color="grey" />
+                            </TouchableOpacity>
+                        </Box>
+                    )
+                }}
+            </Pressable>
+        </Box>
 
+        // <TouchableOpacity onPress={onPress} style={[styles.diary, backgroundColor]}>
+        //     <Box maxW="96" borderWidth="1" borderColor="coolGray.300" shadow="3" bg="coolGray.100" p="5" rounded="8">
+        // <HStack alignItems="center">
+        //     <Badge colorScheme="green" _text={{
+        //         color: "white"
+        //     }} variant="solid" rounded="4">
+        //         {diary.disclosure}
+        //     </Badge>
+        //     <Spacer />
+        //     <Text fontSize={10} color="coolGray.800">
+        //         {diary.user_id} | {diary.date.substr(0, 10)}
+        //     </Text>
+        // </HStack>
+        // <Text color="coolGray.800" mt="3" fontWeight="medium" fontSize="xl">
+        //     {diary.title}
+        // </Text>
+        // <Text mt="2" fontSize="sm" color="coolGray.700" numberOfLines={1} ellipsizeMode='tail' width={'90%'}>
+        //     {content}
+        // </Text>
+        // <TouchableOpacity style={styles.heart}>
+        //     <Ionicons name="md-heart-outline" size={24} color="grey"/>
+        // </TouchableOpacity>
+        //     </Box>
+
+        // </TouchableOpacity>
+    )
 }
 export default FancyDiaryCard;
 
 const styles = StyleSheet.create({
     diary: {
-        marginVertical: 8,
-        marginHorizontal: 16,
+        // marginVertical: 8,
+        // marginHorizontal: 16,
     },
     heart: {
         flex: 1,

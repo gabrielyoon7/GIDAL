3ccb1b7c06f55cf9843c1706ddd8d3c1290815f1
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2022-05-02 13:39:50 +0900

    parallax scroll 적용 시도
    DiaryWriteTestView.js에만 사용

--- app/src/views/test/DiaryWriteTestVeiw.js
+++ app/src/views/test/DiaryWriteTestVeiw.js
@@ -1,6 +1,5 @@
-
 import { Alert, FlatList, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Box, Button, Center, Divider, HStack, Icon } from "native-base"
+import { Box, Button, Center, Divider, HStack, Icon, Image, Avatar, Spacer } from "native-base"
 import React, { useEffect, useState, } from 'react';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import axios from 'axios';
@@ -11,182 +10,84 @@ import RadioDisclosure from "../../components/diary/RadioDisclosure";
 import TagSelector from "../../components/diary/TagSelector";
 import InputContent from "../../components/diary/InputContent";
 import PressableTag from '../../components/tag/interaction/PressableTag';
+import ParallaxScroll from '@monterosa/react-native-parallax-scroll';
+import { useWindowDimensions } from 'react-native';
 
+const ReadProfile = () => {
+  return (
+    <Box>
+      <Text color="coolGray.800" mt="3" fontWeight="bold" fontSize="4xl">
+        일기장
+      </Text>
+      <Text fontSize="sm" color="coolGray.700" my="1">
+          <Text>2022-04-25 00:00:00</Text>
+      </Text>
+      <Box  style={styles.row} justifyContent="center" display="flex">
+          <TouchableOpacity>
+              <HStack alignItems="center" my="1">
+                  <Avatar bg="green.500" alignSelf="center" size="sm" source={{
+                      uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
+                  }}>
+                      AJ
+                  </Avatar>
+                  <Text mx={2} fontSize="md" color="coolGray.700">
+                      202212069
+                  </Text>
+                  <Spacer />
+              </HStack>
+          </TouchableOpacity>
+          <TouchableOpacity style={{ flex: 1, flexDirection: 'row-reverse', alignSelf: 'center', marginLeft: 10}}>
+                      
+          </TouchableOpacity>
+      </Box>
+    </Box>
+  )
+}
+
+const FixedHeader = () => {
+  return (
+    <Box background={'green.300'}>
+      Welcome
+    </Box>
+  )
+}
+
+const FidexBackground = () => {
+  return (
+    <Box background={'darkBlue.300'}>Background</Box>
+  )
+}
+
+const FidexForeground = () => {
+  return (
+    <Box background={'coolGray.300'}>Foreground</Box>
+  )
+}
 
 const DiaryWriteView = (props) => {
-
-  const [Date, setDate] = useState(props.selectedDate);
-  const [Title, setTitle] = useState('');
-  const [Content, setContent] = useState('');
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const [disclosure, setDisclosure] = React.useState('public');
-  const [tags, setTags] = useState([]);
-  const [ref, setRef] = useState(null);
-
-  const selectTags = (selectedTag) => {
-    let newSet = tags;
-    if (newSet.includes(selectedTag)) {
-      const idx = newSet.indexOf(selectedTag)
-      if (idx > -1) {
-        newSet.splice(idx, 1)
-      }
-      setTags([...newSet]); //리렌더링 사용시 매우 중요함
-    } else {
-      newSet.push(selectedTag);
-      setTags([...newSet]); //리렌더링 사용시 매우 중요함
-    }
-    console.log(tags)
-  }
-
-  const showDatePicker = () => {
-    setDatePickerVisibility(true);
-  };
-
-  const hideDatePicker = () => {
-    setDatePickerVisibility(false);
-  };
-
-  const handleConfirm = (date) => {
-    console.warn("dateFormat: ", date.format("yyyy/MM/dd"));
-    hideDatePicker();
-    setDate(date.format("yyyy-MM-dd"))
-  };
-
-  const saveDiary = () => {
-    const user_Id = props.navigation.getState().routes[1].params.user_Id
-    console.log('selected tags : ' + tags);
-    axios.post(config.ip + ':5000/diariesRouter/save', {
-      data: {
-        user_id: user_Id,
-        date: Date,
-        title: Title,
-        content: Content,
-        disclosure: disclosure,
-        tags: tags,
-      }
-    }).then((response) => {
-      if (response.data.status === 'success') {
-        props.navigation.pop();
-        // 스택 쌓지 않고 화면 이동 => 읽기 페이지에서 뒤로가기하면 리스트 페이지 뜸
-      }
-    }).catch(function (error) {
-      console.log(error);
-    })
-  }
-
-  const WriteDiaryHeader = () => {
-    return (
-      <Box style={styles.row} justifyContent="center" display="flex" >
-        <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ flex: 1, flexDirection: 'row', alignSelf: 'center' }} >
-          <AntDesign style={styles.allowIcon} name="left" size={24} color="black" />
-        </TouchableOpacity>
-        <Text style={styles.dateText} onPress={showDatePicker}>{Date}</Text>
-        <Box alignItems="center">
-          <Button onPress={() => { saveDiary(); }} colorScheme="green">작성하기</Button>
-        </Box>
-      </Box>
-    )
-  }
-
-  // useEffect(() => { console.log("component did mount with useEffect!"); }, [tags]);
-
-  // const SelectedTagsView = () => {
-  //   return (
-  //     tags.map((tag) => (
-  //       <PressableTag key={tag} tag={tag} selectTags={selectTags} styles={buttonStyles} />
-  //     ))
-  //   )
-  // }
-  
-  const renderItem = ({ item }) => {
-    return (
-      <PressableTag key={item} tag={item} selectTags={selectTags} styles={buttonStyles} />
-    );
-  };
-
   return (
-    <>
-      <View style={{ backgroundColor: 'white' }}>
-        <WriteDiaryHeader />
-        <Divider />
-        <ScrollView style={{ backgroundColor: 'white' }}>
-          {/* <ScrollView
-            horizontal={true}
-          >
-            <SelectedTagsView />
-          </ScrollView> */}
-          <FlatList
-            horizontal={true}
-            data={tags}
-            ref={(ref) => {
-              setRef(ref);
-            }}
-            renderItem={renderItem}
-            keyExtractor={(item) => item}
-            extraData={tags}
-          />
-
-          <Box style={styles.row} justifyContent="center" display="flex">
-            <TagSelector selectTags={selectTags} />
-          </Box>
-          <Divider />
-          <InputTitle setTitle={setTitle} Title={Title} />
-          {/* <Divider /> */}
-          <InputContent setContent={setContent} content={Content} />
-          <DateTimePickerModal
-            isVisible={isDatePickerVisible}
-            mode="date"
-            onConfirm={handleConfirm}
-            onCancel={hideDatePicker}
-          />
-
-
-        </ScrollView>
-        <RadioDisclosure disclosure={disclosure} setDisclosure={setDisclosure} />
-      </View>
-    </>
+     <ParallaxScroll
+      renderHeader={() => <ReadProfile/>} 
+      parallaxHeaderHeight={300}
+      isHeaderFixed={false}
+      parallaxHeight={250}
+      renderParallaxBackground={() => <FidexBackground />}
+      renderParallaxForeground={() => <FidexForeground />}
+      parallaxBackgroundScrollSpeed={5}
+      parallaxForegroundScrollSpeed={2.5}
+    >
+    </ParallaxScroll>
   );
 };
 
 export default DiaryWriteView;
 
 const styles = StyleSheet.create({
-  allowIcon: {
-    width: 'auto',
-  },
-  dateText: {
-    fontSize: 20,
-    flex: 5,
-    flexDirection: 'row',
-    fontWeight: 'bold',
-    width: 'auto',
-    alignSelf: 'center',
-
-  },
   row: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "center",
-    marginVertical: 10,
-    paddingHorizontal: 15,
-  },
-
-});
-
-const buttonStyles = StyleSheet.create({
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingVertical: 8,
-    borderRadius: 100,
-    backgroundColor: 'yellow', //태그버튼색 변경
-    width: 80
-  },
-  btnView: {
-    borderWidth: StyleSheet.hairlineWidth,
-    borderColor: 'black',
-    borderRadius: 100,
-    borderWidth: 1.5,
-    margin: 3,
-  },
-});
+      flexDirection: "row",
+      flexWrap: "wrap",
+      justifyContent: "center",
+      marginBottom: 16,
+      // backgroundColor: 'black'
+    },
+})

2faa341c87e350c6d0367618337f66fe0e2d1172
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-05-03 10:05:40 +0900

    계층에 따른 navigation 데이터 수신이 불가능한 문제를 해결

--- app/src/views/diary/read/DiaryReadView.js
+++ app/src/views/diary/read/DiaryReadView.js
@@ -11,11 +11,37 @@ import { Path } from "react-native-svg";
 import { AntDesign, MaterialIcons, Ionicons } from "@expo/vector-icons";
 import DiaryReadStaggerButton from './DiaryReadStaggerButton';
 import ParallaxScrollView from 'react-native-parallax-scroll-view';
+import { useNavigationState } from '@react-navigation/native';
 
 const DiaryReadView = (props) => {
     // const [date, setSelectedDate] = React.useState(props.selectedDate);
     const { width } = useWindowDimensions();
-    const diary = props.navigation.getState().routes[1].params.diary;
+    const new_routes = useNavigationState(state => state.routes);
+    const defaultData = {
+        "__v": 0,
+        "_id": "626f78c19ee18cdc829a10de",
+        "accessible_user": [],
+        "comments": [],
+        "content": "dafault_content",
+        "date": "2022-05-02T00:00:00.000Z",
+        "disclosure": "private",
+        "likes": 0,
+        "stickers": [],
+        "tags": [],
+        "title": "default_title",
+        "user_id": "error",
+    };
+    const [diary, setDiary] = React.useState(defaultData);
+    React.useEffect(() => {
+        //초기 일기 수신부
+        try {
+            const idx = new_routes.findIndex(r => r.name === "DiaryRead")
+            setDiary(new_routes[idx].params.diary);
+        } catch (error) {
+            // console.log(error);
+        }
+    })
+
     const [likeCount, setLikeCount] = React.useState(diary.likes)
     const [liked, setLiked] = React.useState(false)
     const deleteDiary = () => {
@@ -47,14 +73,14 @@ const DiaryReadView = (props) => {
     const {
         isOpen,
         onToggle
-      } = useDisclose();
+    } = useDisclose();
 
     const pressHeart = () => {
         setLiked(!liked);
     }
 
     React.useEffect(() => {
-        if(liked) {
+        if (liked) {
             setLikeCount(likeCount - 1)
         } else {
             setLikeCount(likeCount + 1)
@@ -74,26 +100,26 @@ const DiaryReadView = (props) => {
 
     const ReadView = () => {
         return (
-        <View style={styles.background}>
+            <View style={styles.background}>
                 <View style={styles.container}>
-                <Box  style={styles.row} justifyContent="center" display="flex">
-                    <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ flex: 1, flexDirection: 'row' }} >
-                        <AntDesign style={styles.allowIcon} name="left" size={24} color="black" />
-                    </TouchableOpacity>
-                    <DiaryReadStaggerButton/>
-                </Box>
+                    <Box style={styles.row} justifyContent="center" display="flex">
+                        <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ flex: 1, flexDirection: 'row' }} >
+                            <AntDesign style={styles.allowIcon} name="left" size={24} color="black" />
+                        </TouchableOpacity>
+                        <DiaryReadStaggerButton />
+                    </Box>
                     <HStack alignItems="center">
-                    <Badge
-                                backgroundColor="emerald.400"
-                                _text={{
-                                    color: "white"
-                                }}
-                                variant="solid"
-                                rounded="4"
-                                key = {diary.disclosure}
-                            >
-                                {diary.disclosure}
-                            </Badge>
+                        <Badge
+                            backgroundColor="emerald.400"
+                            _text={{
+                                color: "white"
+                            }}
+                            variant="solid"
+                            rounded="4"
+                            key={diary.disclosure}
+                        >
+                            {diary.disclosure}
+                        </Badge>
                         <Spacer />
                     </HStack>
                     <Text color="coolGray.800" mt="3" fontWeight="bold" fontSize="4xl">
@@ -102,7 +128,7 @@ const DiaryReadView = (props) => {
                     <Text fontSize="sm" color="coolGray.700" my="1">
                         <Text>{showDate}</Text> <Text>{showTime}</Text>
                     </Text>
-                    <Box  style={styles.row} justifyContent="center" display="flex">
+                    <Box style={styles.row} justifyContent="center" display="flex">
                         <TouchableOpacity onPress={
                             () => props.navigation.navigate('UserProfile', {
                                 user_id: diary.user_id
@@ -121,15 +147,15 @@ const DiaryReadView = (props) => {
                                 <Spacer />
                             </HStack>
                         </TouchableOpacity>
-                        <TouchableOpacity onPress={() => pressHeart()} style={{ flex: 1, flexDirection: 'row-reverse', alignSelf: 'center', marginLeft: 10}}>
-                                    {liked ? <Ionicons name="heart" size={24} color="red"/> : <Ionicons name="md-heart-outline" size={24} color="gray"/>}
+                        <TouchableOpacity onPress={() => pressHeart()} style={{ flex: 1, flexDirection: 'row-reverse', alignSelf: 'center', marginLeft: 10 }}>
+                            {liked ? <Ionicons name="heart" size={24} color="red" /> : <Ionicons name="md-heart-outline" size={24} color="gray" />}
                         </TouchableOpacity>
                     </Box>
-                    
+
 
                     <Divider my="2" />
                     <View style={styles.source}>
-                        <RenderHtml                            
+                        <RenderHtml
                             contentWidth={width}
                             source={source}
                         />
@@ -145,7 +171,7 @@ const DiaryReadView = (props) => {
                                 }}
                                 variant="solid"
                                 rounded="4"
-                                key = {tag}
+                                key={tag}
                             >
                                 {tag}
                             </Badge>
@@ -158,7 +184,7 @@ const DiaryReadView = (props) => {
                     <Spacer />
 
 
-                    <Button style={styles.button}  colorScheme="green" size="md"  onPress={
+                    <Button style={styles.button} colorScheme="green" size="md" onPress={
                         () => props.navigation.replace('DiaryModify', {
                             diary: diary,
                         })
@@ -166,7 +192,7 @@ const DiaryReadView = (props) => {
                         수정
                     </Button>
 
-                    <Button style={styles.button}  colorScheme="green" size="md"  onPress={() => deleteDiary()} >
+                    <Button style={styles.button} colorScheme="green" size="md" onPress={() => deleteDiary()} >
                         삭제
                     </Button>
 
@@ -175,42 +201,20 @@ const DiaryReadView = (props) => {
         );
     }
 
-    {/*<ParallaxScrollView
-      backgroundColor="blue"
-      contentBackgroundColor="pink"
-      parallaxHeaderHeight={300}
-      // renderScrollComponent={() => <Animated.View />}
-      renderScrollComponent={() => <AnimatedCustomScrollView />}
-      renderForeground={() => (
-       <View style={{ height: 300, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text>Hello World!</Text>
-        </View>
-      )}>
-      <View style={{ height: 500 }}>
-        <Text>Scroll me</Text>
-      </View>
-      </ParallaxScrollView>*/}
-
     return (
         <>
-        {/*<ScrollView style={styles.scroll}>
-            <LinearGradient style={styles.header} colors={['#A6A6A6', 'black']} />
-            <Image style={styles.header} source={{uri:'https://t1.daumcdn.net/cfile/blog/99EC04465C9B308326'}}/>
-            <ReadView/>
-        </ScrollView> */}
-
-        <ParallaxScrollView
-        parallaxHeaderHeight={windowHeight * 0.3}
-        renderForeground={() => (
-            <Image
-             style={styles.header} 
-             source={{uri:'https://t1.daumcdn.net/cfile/blog/99EC04465C9B308326'}}
-             alt="Alternate Text"
-            />
-           )}
-        >
-            <ReadView/>
-        </ParallaxScrollView>
+            <ParallaxScrollView
+                parallaxHeaderHeight={windowHeight * 0.3}
+                renderForeground={() => (
+                    <Image
+                        style={styles.header}
+                        source={{ uri: 'https://t1.daumcdn.net/cfile/blog/99EC04465C9B308326' }}
+                        alt="Alternate Text"
+                    />
+                )}
+            >
+                <ReadView />
+            </ParallaxScrollView>
         </>
     )
 }
@@ -243,7 +247,7 @@ const styles = StyleSheet.create({
 
     },
     source: {
-        minHeight: windowHeight *0.4,
+        minHeight: windowHeight * 0.4,
     },
     row: {
         flexDirection: "row",
@@ -251,7 +255,7 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         marginBottom: 16,
         // backgroundColor: 'black'
-      },
+    },
 })
 
 export default DiaryReadView;

f781821314f8fa8b98170c68a3670e966d50e94b
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-05-04 18:23:45 +0900

    태그를 DB화 함

--- server/routes/Tags.js
+++ server/routes/Tags.js
@@ -15,17 +15,17 @@ const TagDataExample = [
         question: '오늘의 식사는?',
         tags: [
             '김밥', '김치찌개', '과일빙수', '떡볶이',
-            '냉면', 
+            '냉면',
             '돈가스', '도시락', '된장찌개',
             '라면',
             '마라탕', '마라샹궈',
-            '분식', '보쌈', 
+            '분식', '보쌈',
             '스파게티', '샌드위치', '순대국', '샐러드', '수제비', '쌀국수',
             '아구찜', '아구탕', '아이스크림', '일식', '와플',
             '중식', '짜장면', '죽', '족발',
-            '치킨', 
+            '치킨',
             '케이크', '크로플', '칼국수',
-            '토스트', 
+            '토스트',
             '피자', '팥빙수',
             '한식', '해물찜', '해물탕', '훠궈', '햄버거',
         ]
@@ -35,12 +35,20 @@ const TagDataExample = [
 
 
 /* GET. */
-router.post('/init', function(req, res, next) {
+router.get('/init', function (req, res, next) {
+    console.log('하이')    
     // 태그데이터 전체삭제 후 새로 추가하기
-    Tag.deleteMany({}, callback).then( function(){
+    Tag.deleteMany({}).then(function () {
         console.log('기존 태그가 전부 삭제됨');
+        Tag.insertMany(TagDataExample).then(function () {
+            console.log('새로운 태그가 업데이트됨');
+            res.json('정상 처리됨')
+        }
+        ).catch((err) => {
+            console.log(err);
+        });
     }
-    ).catch( (err) => {
+    ).catch((err) => {
         console.log(err);
     });
 });
@@ -48,11 +56,11 @@ router.post('/init', function(req, res, next) {
 
 
 /* GET. */
-router.get('/find', function(req, res, next) {
+router.get('/find', function (req, res, next) {
     // 전체 데이터 가져오기
-    Tag.find().then( (tags) => {
+    Tag.find().then((tags) => {
         res.json(tags)
-    }).catch( (err) => {
+    }).catch((err) => {
         console.log(err);
         next(err)
     });

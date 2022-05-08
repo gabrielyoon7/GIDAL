e4fcd49202c4de89662d37a625b49da302fd2b3c
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-05-08 13:00:47 +0900

    통계 페이지에 태그 질문이 뜨도록 함

--- app/src/views/statistics/StatisticsList.js
+++ app/src/views/statistics/StatisticsList.js
@@ -1,15 +1,50 @@
 
-import { Text, View } from "react-native";
-
-
-
-
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { FlatList, Text, View } from "react-native";
+import { config } from "../../../config";
 const StatisticsList = () => {
-    return(
-        <View>
-            <Text>태그 질문 하나당 카드가 하나 와야함</Text>
-        </View>
+    const [items, setItems] = useState([]);
+    const [ref, setRef] = useState(null);
+
+    useEffect(() => {
+        getTags();
+    }, []);
+
+    useEffect(() => {
+        // console.log('123')
+    }, [items]);
+
+    const getTags = () => {
+
+        let result = []
+        axios.get(config.ip + ':5000/tagsRouter/find')
+            .then((response) => {
+                console.log('hi');
+                console.log(response.data);
+                setItems(response.data);
+            }).catch(function (error) {
+                console.log('hi');
+                console.log(error);
+            })
+    }
+
+    const renderItem = ({ item }) => {
+        return (
+            <Text>{item.question}</Text>
+        );
+    };
+
+    return (
+        <FlatList
+            data={items}
+            ref={(ref) => {
+                setRef(ref);
+            }}
+            renderItem={renderItem}
+            keyExtractor={(item) => item._id}
+        // extraData={selectedId}
+        />
     )
 }
 export default StatisticsList;

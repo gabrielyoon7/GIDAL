64373accf19545be4abac5bcf3129566d45db70e
ch-0012 <73748422+ch-0012@users.noreply.github.com>
2022-05-18 02:29:46 +0900

    통계 리스트 카드 모양 변경

--- app/src/components/statistics/StatisticsCard.js
+++ app/src/components/statistics/StatisticsCard.js
@@ -1,28 +1,59 @@
-import { Heading, HStack, Spacer, Text } from "native-base";
+import { Box, Heading, HStack, Pressable, Spacer, Text } from "native-base";
 import { StyleSheet, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 const StatisticsCard = (props) => {
+    // return (
+    //     <TouchableOpacity
+            // onPress={
+            //     () => {
+            //         props.navigation.navigate('UserStatisticsPreview', {
+            //             id: props.id,
+            //             question : props.question,
+            //         })
+            //     }
+            // }
+    //     >
+    //         <View style={styles.card}>
+    //             <HStack>
+    //                 <Text fontSize="xl">{props.question}</Text>
+    //                 <Spacer />
+    //                 <Text>에 대한 통계 보러가기</Text>
+    //             </HStack>
+    //         </View>
+    //     </TouchableOpacity>
+    // );
     return (
-        <TouchableOpacity
-            onPress={
-                () => {
-                    props.navigation.navigate('UserStatisticsPreview', {
-                        id: props.id,
-                        question : props.question,
-                    })
-                }
-            }
+    <Box py="1" px="1"
+        maxW="94%"
+        minW="96%"
+        borderWidth="1"
+        borderColor="coolGray.300"
+        shadow="3"
+        bg={"coolGray.100"}
+        p="5"
+        rounded="8"
+        style={styles.fancyCard}
         >
-            <View style={styles.card}>
-                <HStack>
-                    <Text fontSize="xl">{props.question}</Text>
-                    <Spacer />
-                    <Text>에 대한 통계 보러가기</Text>
-                </HStack>
-            </View>
-        </TouchableOpacity>
-    );
+            <TouchableOpacity
+                        onPress={
+                            () => {
+                                props.navigation.navigate('UserStatisticsPreview', {
+                                    id: props.id,
+                                    question : props.question,
+                                })
+                            }
+                        }>
+                <View style={{paddingHorizontal:10}}>
+                    <HStack>
+                        <Text fontSize="xl" style={styles.cardTitleText}>Q : {props.question}</Text>
+                        <Spacer />
+                        <Text style={styles.goDetailText}>에 대한 통계 보러가기</Text>
+                    </HStack>
+                </View>
+            </TouchableOpacity>
+        </Box>
+    )
 }
 
 export default StatisticsCard;
@@ -37,5 +68,17 @@ const styles = StyleSheet.create({
         padding: 20,
         marginHorizontal: 10,
     },
-
+    fancyCard: {
+        height: 70,
+        alignSelf: 'center',
+        marginVertical: 8,
+    },
+    cardTitleText: {
+        marginTop: 6
+    },
+    goDetailText: {
+        position: 'absolute',
+        top: 35, left: 200,
+        color: 'gray'
+    }
 });

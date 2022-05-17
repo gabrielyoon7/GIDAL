816efc96f845f170933f0a4eebefce2631369138
김가영 <83538598+gykim0923@users.noreply.github.com>
2022-05-17 15:16:16 +0900

    통계 그래프 구현중

--- app/src/views/statistics/tag/detail/PersonalStatDetailView.js
+++ app/src/views/statistics/tag/detail/PersonalStatDetailView.js
@@ -2,17 +2,23 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { Text, View } from "react-native"
 import { config } from "../../../../../config";
+import TagChart from "../../../../components/tag/interaction/TagChart";
+
 
 
 const PersonalStatDetailView = (props) => {
 
     return(
+      
         <View>
             <Text>{props.data.title}</Text>
             {props.tagLogArr.map((tag)=>(
                 <Text>{tag._id}{tag.count}</Text>
             ))}
+           <TagChart/>
         </View>
+     
+       
     )
 }
 

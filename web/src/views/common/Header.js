eed23fded00aaaf1836e5451deaff60a0c2df4a3
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-05-20 09:40:20 +0900

    로그인 했을 때만 메뉴가 뜨도록 변경

--- web/src/views/common/Header.js
+++ web/src/views/common/Header.js
@@ -29,8 +29,8 @@ const Header = () => {
                     <div className="col-4 pt-1">
                         <a className="link-secondary" href="https://github.com/gabrielyoon7/GIDAL">고객센터</a>
                     </div>
-                    <div className="col-4 text-center">
-                        <Link to="/"><div className="blog-header-logo text-dark" href="/">기록의 달인</div></Link>
+                    <div className="col-4 text-center h1">
+                        <Link to="/" className='text-decoration-none'><div className="blog-header-logo text-dark" href="/">기록의 달인</div></Link>
                     </div>
                     {/* <div className="col-4 d-flex justify-content-end align-items-center">
                         <a className="btn btn-sm btn-outline-secondary" href="#">로그인</a>
@@ -59,16 +59,22 @@ const Header = () => {
                     </div>
                 </div>
             </header>
-            <div className="nav-scroller py-1 mb-2">
-                {/* 이 부분은 로그인 한 사람에게만 뜨면 좋겠음 */}
-                <nav className="nav d-flex justify-content-between">
-                    <a className="p-2 link-secondary" href="#">마이다이어리</a>
-                    <a className="p-2 link-secondary" href="sns">피드</a>
-                    <a className="p-2 link-secondary" href="#">통계</a>
-                    <a className="p-2 link-secondary" href="#">할일</a>
-                    <a className="p-2 link-secondary" href="#">설정</a>
-                </nav>
-            </div>
+            {
+                isLogin
+                    ?
+                    <div className="nav-scroller py-1 mb-2">
+                        {/* 이 부분은 로그인 한 사람에게만 뜨면 좋겠음 */}
+                        <nav className="nav d-flex justify-content-between">
+                            <a className="p-2 link-secondary" href="#">마이다이어리</a>
+                            <a className="p-2 link-secondary" href="sns">피드</a>
+                            <a className="p-2 link-secondary" href="#">통계</a>
+                            <a className="p-2 link-secondary" href="#">할일</a>
+                            <a className="p-2 link-secondary" href="#">설정</a>
+                        </nav>
+                    </div>
+                    :
+                    <div></div>
+             }
             {/* <!-- Login Modal --> */}
             <div className="modal fade" id="loginModal" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
                 <div className="modal-dialog">

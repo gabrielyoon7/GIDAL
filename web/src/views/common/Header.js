92e00f4aef41fe7a5368ce7b9e4fe68d633249e3
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-05-20 14:57:00 +0900

    Header 디자인 변경

--- web/src/views/common/Header.js
+++ web/src/views/common/Header.js
@@ -52,7 +52,15 @@ const Header = () => {
                             isLogin
                                 ?
                                 <div>
-                                    <a className="p-2 link-secondary" href="#">{userId}</a>님 안녕하세요
+                                    <div className="dropdown">
+                                        <button className="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                                            <i class="bi bi-person-circle"></i>
+                                        </button>
+                                        <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+                                            <li><a className="dropdown-item" href="#">{userId}</a></li>
+                                            <li><a className="dropdown-item" onClick={() => logout()}>로그아웃</a></li>
+                                        </ul>
+                                    </div>
                                     {/* <button type="button" className="btn btn-sm btn-outline-secondary" onClick={() => logout()}>
                                         로그아웃
                                     </button> */}

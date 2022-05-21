b6ec87d57b539e3de430ce77b33ce51524caba77
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-05-21 16:08:05 +0900

    툴바 디자인 변경

--- app/src/components/diary/RadioDisclosure.js
+++ app/src/components/diary/RadioDisclosure.js
@@ -2,16 +2,16 @@ import { Text, View, HStack } from "native-base";
 import { RadioButton } from "react-native-paper";
 
 const RadioDisclosure = (props) => {
-    return (
-      <View  style={{backgroundColor:'white'}} >
+  return (
+    <View style={{ backgroundColor: 'white' }} >
       <RadioButton.Group onValueChange={newValue => props.setDisclosure(newValue)} value={props.disclosure}>
         <HStack justifyContent="center" md="center">
-          <RadioButton.Item label="전체공개" value="public"/>
-          <RadioButton.Item label="나만보기" value="private"/>
-          <RadioButton.Item label="친구공개" value="friend"/>
+          <RadioButton.Item label="전체공개" value="public" />
+          <RadioButton.Item label="나만보기" value="private" />
+          <RadioButton.Item label="친구공개" value="friend" />
         </HStack>
       </RadioButton.Group>
-      </View>
-    )
+    </View>
+  )
 }
 export default RadioDisclosure;

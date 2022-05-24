500115f2cd518bc9eb8ab984e1c615bf6ae2898a
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-05-24 16:28:07 +0900

    백버튼 오류 수정

--- app/src/components/common/BackButton.js
+++ app/src/components/common/BackButton.js
@@ -1,10 +1,13 @@
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { AntDesign } from "@expo/vector-icons";
+import { Box } from "native-base";
 
 const BackButton = (props) => {
     return (
         <TouchableOpacity onPress={() => props.navigation.goBack()} style={{ paddingLeft: 10, paddingTop: 15 }} >
-            <AntDesign name="left" size={24} color="white" />
+            <Box mb={3}>
+                <AntDesign name="left" size={24} color="black" />
+            </Box>
         </TouchableOpacity>
     )
 }

099fcd7c5c20099d1322edbbc75d6529bf58e759
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-05-24 16:53:06 +0900

    친구 통계 디테일 개선

--- app/src/views/statistics/tag/detail/FriendsStatDetailView.js
+++ app/src/views/statistics/tag/detail/FriendsStatDetailView.js
@@ -1,23 +1,48 @@
 import axios from "axios";
+import { Box, Heading } from "native-base";
 import { useEffect, useState } from "react";
-import { Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
 import { config } from "../../../../../config";
 
 
 const FriendsStatDetailView = (props) => {
 
-    return(
-        <View>
-            <Text>{props.data.title}</Text>
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            paddingTop: 22
+        },
+        sectionHeader: {
+            paddingTop: 2,
+            paddingLeft: 10,
+            paddingRight: 10,
+            paddingBottom: 2,
+            fontSize: 14,
+            fontWeight: 'bold',
+            backgroundColor: 'rgba(247,247,247,1.0)',
+        },
+        item: {
+            padding: 10,
+            fontSize: 18,
+            height: 44,
+        },
+    })
+
+    return (
+        <Box p={5} >
+            <Heading mb={5} >{props.data.title}</Heading>
+
             {props.tagLogArr && props.tagLogArr.map((friend) => (
-                <>
-                    <Text>{friend.id}</Text>
+                <Box my={3}>
+                    <Text style={styles.sectionHeader}>{friend.id}</Text>
+                    {/* <Text></Text> */}
                     {friend.statistics.map((tag) => (
-                        <Text key={tag._id}>{tag._id}{tag.count}</Text>
+                        <Text style={styles.item} key={tag._id}>{tag._id} {tag.count}</Text>
                     ))}
-                </>
+                </Box>
             ))}
-        </View>
+        </Box>
     )
 }
 

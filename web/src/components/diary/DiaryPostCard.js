92e814c16f7c2abdcf9821a9f3d7ee0db934b025
soyoung125 <sypark041754@gmail.com>
2022-05-25 23:34:49 +0900

    다이어리 클릭 시 읽기 페이지로 이동 구현

--- web/src/components/diary/DiaryPostCard.js
+++ web/src/components/diary/DiaryPostCard.js
@@ -8,13 +8,16 @@ const DiaryPostCard = (props) => {
         content = content.replace(/&nbsp;/gi, "");
         // 정규식을 이용한 HTML 태그 제거 끝
 
+        const readDiary = (e) => {
+            window.location.href = "/read"
+        }
     return (
-        <article className="blog-post">
+        <div className="blog-post" onClick={readDiary}>
             <h2 className="blog-post-title">{props.diary.title}</h2>
             <p className="blog-post-meta">{props.diary.date} by <a href={"user/"+props.diary.user_id}>{props.diary.user_id}</a></p>
             <p>{content}</p>
             <hr></hr>
-        </article>
+        </div>
     )
 }
 export default DiaryPostCard;

2e6fe1aa2c74761d3a8b821db3ad057f8460ae91
soyoung125 <sypark041754@gmail.com>
2022-05-26 00:26:52 +0900

    선택한 다이어리에 맞는 내용 띄우기 구현 중 findOne api 수정해야 함

--- web/src/components/diary/DiaryPostCard.js
+++ web/src/components/diary/DiaryPostCard.js
@@ -1,20 +1,17 @@
-const DiaryPostCard = (props) => {
+const DiaryPostCard = ({diary, readDiary}) => {
 
         // 정규식을 이용한 HTML 태그 제거 시작
-        let content = props.diary.content.replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, "");
+        let content = diary.content.replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, "");
         content = content.replace(/<br\/>/ig, "\n");
         content = content.replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, "");
         content = content.replace(/(<([^>]+)>)/gi, "");
         content = content.replace(/&nbsp;/gi, "");
         // 정규식을 이용한 HTML 태그 제거 끝
 
-        const readDiary = (e) => {
-            window.location.href = "/read"
-        }
     return (
-        <div className="blog-post" onClick={readDiary}>
-            <h2 className="blog-post-title">{props.diary.title}</h2>
-            <p className="blog-post-meta">{props.diary.date} by <a href={"user/"+props.diary.user_id}>{props.diary.user_id}</a></p>
+        <div className="blog-post" onClick={readDiary} >
+            <h2 className="blog-post-title">{diary.title}</h2>
+            <p className="blog-post-meta">{diary.date} by <a href={"user/"+diary.user_id}>{diary.user_id}</a></p>
             <p>{content}</p>
             <hr></hr>
         </div>
--- web/src/views/diary/read/DiaryReadView.js
+++ web/src/views/diary/read/DiaryReadView.js
@@ -1,4 +1,27 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from 'axios';
+
 const DiaryReadView = () => {
+    const params = useParams();
+    const diary_id = params.id;
+
+    useEffect(() => {
+        console.log(diary_id)
+        getItem();
+    }, [])
+
+    const getItem = () => {
+        axios.post('/diariesRouter/findOne', {
+            id: diary_id,
+        }).then((response) => {
+            console.log(response)
+            // setComments(response.data);
+        }).catch(function (error) {
+            console.log(error);
+        })
+    }
+
     return(
         <div>ㅇㅇ</div>
     )

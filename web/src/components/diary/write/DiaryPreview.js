7bf6d4daf91cc78eb6a7f738451e4d0a054145b8
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-05-27 09:35:08 +0900

    preview 꾸미기

--- web/src/components/diary/write/DiaryPreview.js
+++ web/src/components/diary/write/DiaryPreview.js
@@ -1,15 +1,32 @@
 const DiaryPreview = (props) => {
     return (
-        <div className='diaryPreview my-3'>
+        <div className='diaryPreview my-3 '>
             <div className="fw-bold h3">
                 일기 미리보기
             </div>
-            <div>
-                {props.title}
+            <div className='card rounded-3' style={style} >
+                {/* <!-- 상태바 시작--> */}
+                <div class="d-flex justify-content-between pt-2 px-4" id="status-bar">
+                    <div><b>12:00</b></div>
+                    <div>
+                        <i class="bi bi-bluetooth"></i>
+                        <i class="bi bi-bar-chart-fill"></i>
+                        <i class="bi bi-wifi"></i>
+                        <i class="bi bi-battery-full"></i>
+                    </div>
+                </div>
+                {/* <!-- 상태바 끝--> */}
+                <div>
+                    {props.title}
+                </div>
+                <div className="preview" dangerouslySetInnerHTML={props.createMarkup(props.convertedContent)}></div>
             </div>
-            <div className="preview" dangerouslySetInnerHTML={props.createMarkup(props.convertedContent)}></div>
         </div>
     )
 }
 
 export default DiaryPreview;
+
+const style = { 
+    maxWidth:"400px"
+}

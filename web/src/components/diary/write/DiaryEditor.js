61f464e22b76c39bfc37aa0e28591afe330b0a50
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-05-29 00:43:27 +0900

    .

--- web/src/components/diary/write/DiaryEditor.js
+++ web/src/components/diary/write/DiaryEditor.js
@@ -20,12 +20,20 @@ const DiaryEditor = (props) => {
             </div>
         )
     }
+    const TagSelector =()=>{
+        return(
+            <div>
+                
+            </div>
+        )
+    }
     return (
         <div className='diaryEditor my-3'>
             <div className="fw-bold h3">
                 일기 작성하기
             </div>
-            <div className="input-group mb-3">
+            <TagSelector/>
+            <div className="input-group my-3">
                 <span className="input-group-text" id="basic-addon1">제목</span>
                 <input type="text" className="form-control" placeholder="제목을 입력하세요" aria-label="제목을 입력하세요" aria-describedby="basic-addon1" onChange={props.handleTitleChange} />
             </div>

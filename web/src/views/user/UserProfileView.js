ae67022dfc68ae9b72de1306580c0d8060308ca4
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-05-31 02:24:48 +0900

    웹 사용자 프로필 초안 작성

--- web/src/views/user/UserProfileView.js
+++ web/src/views/user/UserProfileView.js
@@ -84,8 +84,12 @@ const UserProfileView = () => {
                         <div className="position-sticky">
                             {/* <div> */}
                             <div className="p-4 mb-3 bg-light rounded">
-                                <h4 className="fw-bold">{user_id}</h4>
-                                <p className="mb-0">개인 정보가 나와야 할 자리</p>
+                                <div className="col text-center">
+                                    <svg className="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777" /><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
+                                    <h2>{user_id}</h2>
+                                    <p>팔로우 00명 팔로워 00명</p>
+                                    <p><a className="btn btn-secondary" href="#">View details &raquo;</a></p>
+                                </div>
                             </div>
 
                             <div className="p-4">

5834cedc5318af9334f1ba40db88cd3ae55945cf
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-05-30 21:09:48 +0900

    웹 마이 페이지 개선

--- web/src/views/user/UserProfileView.js
+++ web/src/views/user/UserProfileView.js
@@ -4,11 +4,30 @@ import { useParams } from "react-router-dom";
 import DiaryPostCard from "../../components/diary/DiaryPostCard";
 import Footer from "../common/Footer";
 import Header from "../common/Header";
+import DiaryMobileReadView from "../diary/read/DiaryMobileReadView";
 
 const UserProfileView = () => {
     const params = useParams();
     const user_id = params.id;
     const [items, setItems] = useState([]);
+
+
+    const defaultData = {
+        "__v": 0,
+        "_id": "626f78c19ee18cdc829a10de",
+        "accessible_user": [],
+        "comments": [],
+        "content": "loading...",
+        "date": "2022-05-02T00:00:00.000Z",
+        "disclosure": "private",
+        "likes": 0,
+        "stickers": [],
+        "tags": [],
+        "likers": [],
+        "title": "loading...",
+        "user_id": "loading...",
+    };
+    const [diary, setDiary] = useState(defaultData);
 
     useEffect(() => {
         getitems();
@@ -40,11 +59,18 @@ const UserProfileView = () => {
                 <div className="row g-5">
                     <div className="col-md-8">
                         <h3 className="pb-4 mb-4 fw-bold border-bottom">
-                        {user_id}의 일기
+                            {user_id}의 일기
                         </h3>
-                        {items.map((diary) => (
-                            <DiaryPostCard diary={diary} key={diary._id} />
-                        ))}
+
+                        <div className="row">
+                            {items.map((diary) => (
+                                <div className="col-xl-6 text-truncate" key={diary._id}>
+                                    <a href="#" className="text-decoration-none text-dark" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={() => setDiary(diary)}>
+                                        <DiaryPostCard diary={diary} />
+                                    </a>
+                                </div>
+                            ))}
+                        </div>
 
 
                         <nav className="blog-pagination" aria-label="Pagination">
@@ -92,6 +118,16 @@ const UserProfileView = () => {
                     </div>
                 </div>
             </main>
+            {/* <!-- Modal --> */}
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content" style={{ width: '430px' }}>
+                        <div className="modal-body bg-dark rounded-3">
+                            <DiaryMobileReadView diary={diary} />
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Footer />
         </div>
     )

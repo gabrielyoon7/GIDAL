929b98b1380f8b1856937efe18734406f83b4e4c
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-06-08 16:55:24 +0900

    web의 모바일 뷰에서 댓글/좋아요 수가 연동되도록 수정

--- web/src/components/common/MobileUI.js
+++ web/src/components/common/MobileUI.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Ad from "./Ad";
 
 const MobileUI = (props) => {
     const [time, setTime] = useState(new Date());
@@ -21,8 +22,8 @@ const MobileUI = (props) => {
             </div>
         )
     }
-    const BackButtonHeader =()=>{
-        return(
+    const BackButtonHeader = () => {
+        return (
             <div className="px-3 py-2">
                 <a href="#">
                     <i className="bi bi-chevron-left"></i>
@@ -31,34 +32,42 @@ const MobileUI = (props) => {
         )
     }
     return (
-        <div className='card rounded-3' style={props.style} >
-            {/* <!-- 상태바 시작--> */}
-            <MobileHeader/>
-            {/* <!-- 상태바 끝--> */}
-            <BackButtonHeader/>
-            <div>
-                <img src='https://t1.daumcdn.net/cfile/blog/99EC04465C9B308326' style={{ height: '200px', width: '400px' }} />
-            </div>
-            <div className="p-3">
-                <button className="btn btn-success btn-sm">{props.disclosure}</button>
-                <div className="fs-2 fw-bold">
-                    {props.title ? props.title : '제목을 입력하세요'}
+        <>
+            <div className='card rounded-3' style={props.style} >
+                {/* <!-- 상태바 시작--> */}
+                <MobileHeader />
+                {/* <!-- 상태바 끝--> */}
+                <BackButtonHeader />
+                <div>
+                    <img src='https://t1.daumcdn.net/cfile/blog/99EC04465C9B308326' style={{ height: '200px', width: '400px' }} />
                 </div>
-                <div className="my-1">
-                    {props.time}
-                </div>
-                <div className="my-1">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" className="me-2 rounded-circle" alt="Avatar" style={{ height: '30px' }} />
-                    <strong className="d-inline-block mb-2 text-primary">{props.userId}</strong>
+                <div className="p-3">
+                    <button className="btn btn-success btn-sm">{props.disclosure}</button>
+                    <div className="fs-2 fw-bold">
+                        {props.title ? props.title : '제목을 입력하세요'}
+                    </div>
+                    <div className="my-1">
+                        {props.time}
+                    </div>
+                    <div className="my-1">
+                        <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" className="me-2 rounded-circle" alt="Avatar" style={{ height: '30px' }} />
+                        <strong className="d-inline-block mb-2 text-primary">{props.userId}</strong>
+                        <hr></hr>
+                    </div>
+                    <div className="preview" dangerouslySetInnerHTML={props.createMarkup(props.convertedContent)} style={{ minHeight: '300px' }}></div>
+                    <div className="d-flex justify-content-end">
+                        <a className="text-decoration-none text-dark" href="#" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                            댓글 {props.comments}개 좋아요 {props.likes}개
+                        </a>
+                    </div>
                     <hr></hr>
                 </div>
-                <div className="preview" dangerouslySetInnerHTML={props.createMarkup(props.convertedContent)} style={{ minHeight: '300px' }}></div>
-                <div className="d-flex justify-content-end">
-                    댓글 {props.comments}개 좋아요 {props.likes}개
-                </div>
-                <hr></hr>
             </div>
-        </div>
+            {/* <!-- Modal --> */}
+            <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <Ad />
+            </div>
+        </>
     )
 }
 
--- web/src/views/diary/read/DiaryMobileReadView.js
+++ web/src/views/diary/read/DiaryMobileReadView.js
@@ -1,6 +1,4 @@
-import axios from "axios";
 import DOMPurify from "dompurify";
-import { useEffect, useState } from "react";
 import MobileUI from "../../../components/common/MobileUI";
 
 const DiaryMobileReadView = (props) => {
@@ -13,22 +11,20 @@ const DiaryMobileReadView = (props) => {
 
     return (
         <>
-            {
-                <MobileUI
-                    style={{
-                        width: "400px",
-                        minHeight: "800px"
-                    }}
-                    disclosure={props.diary.disclosure}
-                    title={props.diary.title}
-                    time={props.diary.date.slice(0,10)}
-                    userId={props.diary.user_id}
-                    convertedContent={props.diary.content}
-                    createMarkup={createMarkup}
-                    likes={99}
-                    comments={99}
-                />
-            }
+            <MobileUI
+                style={{
+                    width: "400px",
+                    minHeight: "800px"
+                }}
+                disclosure={props.diary.disclosure}
+                title={props.diary.title}
+                time={props.diary.date.slice(0, 10)}
+                userId={props.diary.user_id}
+                convertedContent={props.diary.content}
+                createMarkup={createMarkup}
+                likes={props.diary.likers.length}
+                comments={props.diary.comments.length}
+            />
         </>
 
     )
